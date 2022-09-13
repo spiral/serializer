3cee252c24293352fe90067655f45c9ba2293d8e
spiralbot <ci-bot@spiralscout.com>
2022-09-13 07:26:12 +0000

    Merge pull request #787 from spiral/hotfix/composer

---
+++ src/Bootloader/SerializerBootloader.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Serializer\Bootloader;
+
+use Psr\Container\ContainerInterface;
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\EnvironmentInterface;
+use Spiral\Config\ConfiguratorInterface;
+use Spiral\Core\Container\Autowire;
+use Spiral\Core\FactoryInterface;
+use Spiral\Serializer\Config\SerializerConfig;
+use Spiral\Serializer\Serializer\JsonSerializer;
+use Spiral\Serializer\Serializer\PhpSerializer;
+use Spiral\Serializer\SerializerInterface;
+use Spiral\Serializer\SerializerRegistry;
+use Spiral\Serializer\SerializerManager;
+use Spiral\Serializer\SerializerRegistryInterface;
+
+final class SerializerBootloader extends Bootloader
+{
+    protected const SINGLETONS = [
+        SerializerManager::class => [self::class, 'initSerializerManager'],
+        SerializerRegistryInterface::class => [self::class, 'initSerializerRegistry'],
+        SerializerInterface::class => SerializerManager::class,
+    ];
+
+    public function __construct(
+        private readonly ConfiguratorInterface $config,
+        private readonly ContainerInterface $container,
+        private readonly FactoryInterface $factory
+    ) {
+    }
+
+    public function init(EnvironmentInterface $env): void
+    {
+        $this->initConfig($env);
+    }
+
+    /**
+     * @noRector RemoveUnusedPrivateMethodRector
+     */
+    private function initSerializerManager(
+        SerializerRegistryInterface $serializers,
+        SerializerConfig $config
+    ): SerializerManager {
+        return new SerializerManager($serializers, $config->getDefault());
+    }
+
+    /**
+     * @noRector RemoveUnusedPrivateMethodRector
+     */
+    private function initSerializerRegistry(SerializerConfig $config): SerializerRegistryInterface
+    {
+        return new SerializerRegistry(\array_map([$this, 'wire'], $config->getSerializers()));
+    }
+
+    private function initConfig(EnvironmentInterface $env): void
+    {
+        $this->config->setDefaults(SerializerConfig::CONFIG, [
+            'default' => $env->get('DEFAULT_SERIALIZER_FORMAT', SerializerConfig::DEFAULT_SERIALIZER),
+            'serializers' => [
+                'json' => new JsonSerializer(),
+                'serializer' => new PhpSerializer(),
+            ],
+        ]);
+    }
+
+    /**
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    private function wire(string|Autowire|SerializerInterface $serializer): SerializerInterface
+    {
+        return match (true) {
+            $serializer instanceof SerializerInterface => $serializer,
+            $serializer instanceof Autowire => $serializer->resolve($this->factory),
+            default => $this->container->get($serializer)
+        };
+    }
+}
